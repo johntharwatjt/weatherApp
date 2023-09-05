05ca02f8958dd9007a4f5c4b70f77c2082f0a57a
johntharwatjt <john.tharwat.jt@gmail.com>
2023-09-05 15:33:22 +0300

    darkmode

--- script.js
+++ script.js
@@ -178,7 +178,7 @@ darkMode.addEventListener("click", () => {
     img.src = "assets/Logo.svg";
     darkLight.src = "assets/light-mode.svg";
   } else {
-    img.src = "assets/logo-darkmode.svg";
+    img.src = "assets/logo-darkmode.png";
     darkLight.src = "assets/dark-mode.svg";
   }
 });
